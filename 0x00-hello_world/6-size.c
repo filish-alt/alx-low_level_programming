89f1a678fda75d3bc92cd368a6efb7313eb4ee95
filagot <filagotshewafera@gmail.com>
2022-03-10 21:28:39 +0300

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return:Always 0(Sucess)
+*/
+int main(void)
+{
+printf("size of a char: ", sizeof(char));
+printf("size of an int: ", sizeof(int));
+printf("size of a long int: ", sizeof(long int));
+printf("size of a long long int: ", sizeof(long long int));
+printf("size of a float", sizeof(float));
+return (0)
+}
