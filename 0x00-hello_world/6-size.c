f1022827fa85d612e6f1a66825b024c0b34f91ca
filagot <filagotshewafera@gmail.com>
2022-03-10 21:39:37 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-printf("size of a char: ", sizeof(char));
-printf("size of an int: ", sizeof(int));
-printf("size of a long int: ", sizeof(long int));
-printf("size of a long long int: ", sizeof(long long int));
-printf("size of a float", sizeof(float));
+printf("size of a char: %d byte(s) \n",sizeof(char));
+printf("size of an int: %d byte(s) \n",sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s) \n", sizeof(long long int));
+printf("size of a float: %d byte(s) \n", sizeof(float));
 return (0)
 }
