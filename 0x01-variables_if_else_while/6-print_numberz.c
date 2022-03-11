dc206c6f8521958dcd2ad11a0d399407c19242b6
filagot <filagotshewafera@gmail.com>
2022-03-11 22:56:58 +0300

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+table File  18 lines (17 sloc)  210 Bytes
+   
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return:Always 0(Sucess)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar("i");
+}
+putchar('\n');
+return (0);
+}
