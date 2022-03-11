d2a776647fc1a356526ea0f57d1ff3fc6f06ee41
filagot <filagotshewafera@gmail.com>
2022-03-11 22:24:04 +0300

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return:Always 0(Sucess)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf(i);
+}
+print("\n");
+return (0);
+}
