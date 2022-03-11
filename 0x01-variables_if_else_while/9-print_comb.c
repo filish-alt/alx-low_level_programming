82e30fe5d05c3069fbbaf7d02d04ca149e7661b1
filagot <filagotshewafera@gmail.com>
2022-03-12 00:29:24 +0300

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return:Always 0(Sucess)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
