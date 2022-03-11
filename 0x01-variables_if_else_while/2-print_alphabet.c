b1998116dca4a3561be8d36709fd6867b236e7d5
filagot <filagotshewafera@gmail.com>
2022-03-11 19:57:06 +0300

    lastdigit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return:Always 0(Sucess)
+*/
+int main(void)
+{
+char i;
+for (i='a'; i<='z'; i++)
+putchar("%c ",i);
+return (0);
+}
+
