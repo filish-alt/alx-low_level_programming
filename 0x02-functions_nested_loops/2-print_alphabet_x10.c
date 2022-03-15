6163826ae16e80c47c6ab952574aefae722d6063
filagot <filagotshewafera@gmail.com>
2022-03-15 23:49:25 +0300

    lowercase10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /*
- *print_alphabet_x10 print alphapet lowercase 10times
+ *print_alphabet_x10 - print alphapet lowercase 10times
  *
- */
+ **/
 void print_alphabet_x10(void)
 {
 char c;
 int i;
-for (i = 0; c <= 10; c++)
+for (i = 0; i <= 10; i++)
 {
-for (c = 'a'; c < 'z'; i++)
+for (c = 'a'; c < 'z'; c++)
 {
 _putchar(c);
 }
