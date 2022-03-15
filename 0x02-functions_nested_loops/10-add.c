ddd4e6cf56db607357f7c9e43d9398ca3b00362b
filagot <filagotshewafera@gmail.com>
2022-03-16 01:35:15 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers together
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: a + b result
+ */
+int add(int, int)
+{
+int i, j,add;
+add = i + j;
+return (add);
