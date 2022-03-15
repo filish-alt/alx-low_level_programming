ea979502cdcd243b310a7aa30c1611efaab0d33f
filagot <filagotshewafera@gmail.com>
2022-03-15 23:30:21 +0300

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
  *main -Entry point
  *Return:Always 0(Sucess)
 */
-int main(void)
+void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
@@ -11,5 +11,4 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-return (0);
 }
