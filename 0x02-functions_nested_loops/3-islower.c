7021d57f9bcdd786b4ea7cf5ae6fce5fd91593c0
filagot <filagotshewafera@gmail.com>
2022-03-16 00:03:41 +0300

    lowercaseorupper

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * -islower - is print lower case
+ *  return : 0 for uppercase and 1 for lower case
+ *
+ **/
+int _islower(int c)
+{
+char c;
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
