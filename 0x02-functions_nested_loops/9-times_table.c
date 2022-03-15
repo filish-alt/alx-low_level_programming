12440412750db9cc30e1255cff1f78ea93bd5e32
filagot <filagotshewafera@gmail.com>
2022-03-16 01:29:43 +0300

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,20 @@ if (j == 0)
 			{
 _putchar(k + '0');
 			}
-else 
+else if (k <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
+else if (k >= 10)
+			{
+_putchar(',');
+_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+			}
 }
 _putchar('\n');
 	}
