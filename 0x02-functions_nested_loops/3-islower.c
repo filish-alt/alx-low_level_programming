1559a27a407436dba0c4d31aef3734d9d82c6735
filagot <filagotshewafera@gmail.com>
2022-03-16 00:10:33 +0300

    lowercaseorupper

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -islower -check if it's lower case and print lower case
+ * _islower -check if it's lower case and print lower case
  *  return : 0 for uppercase and 1 for lower case
  *
  **/
