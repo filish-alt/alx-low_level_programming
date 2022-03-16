5e7a08a0a107c1e12bed75c87bad68f4d408b1a4
filagot <filagotshewafera@gmail.com>
2022-03-17 00:00:45 +0300

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+postitive_or_negative(i);
+return (0);
+}
