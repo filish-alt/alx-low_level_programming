7ff9a3b747cd81746330df9db80094def0fcee21
filagot <filagotshewafera@gmail.com>
2022-03-18 00:07:12 +0300

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - return 1 if its digit and 0 otherwise
+ * @c: take in integet
+ * Return: Always 0.
+ **/
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
