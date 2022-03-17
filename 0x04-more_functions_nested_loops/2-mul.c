30e47f9991f8a58db2bf6c6068b5c35eb4b4d6be
filagot <filagotshewafera@gmail.com>
2022-03-18 00:19:30 +0300

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - return multiplication of two number
+ * @a: take in integer
+ * @b: take in integer
+ * Return: Always 0.
+ **/
+int mul(int a, int b)
+{
+return (a * b);
+}
