23cdcb34eba5ca9c49649d7cc62cb399f315c31d
filagot <filagotshewafera@gmail.com>
2022-03-18 23:04:39 +0300

    number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *more_numbers - print number
+ *Return : void
+ **/
+void more_numbers(void)
+{
+char i , j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+_putchar('0' + j);
+}
+_putchar('\n');
+}
+}
