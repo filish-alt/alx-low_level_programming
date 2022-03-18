10d700ac04616034ae83caea500cafc01db87ba8
filagot <filagotshewafera@gmail.com>
2022-03-18 22:33:16 +0300

    number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - print numbers
@@ -9,7 +8,7 @@
  **/
 int print_numbers(void)
 {
-int i;
+char i;
 for (i = 0; i <= 9; i++)
 {
 _putchar(i + '0');
