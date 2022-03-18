8be038b466432ef02111394971d6c5668951a11c
filagot <filagotshewafera@gmail.com>
2022-03-18 23:15:07 +0300

    number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,15 @@
  **/
 void more_numbers(void)
 {
-char j , c;
-int i;
-for (i = 0; i < 10; i++)
+char n, c;
+int i = 0;
+
+while (i < 10)
 {
-for (j = 0; j <= 14; j++)
+for (n = 0; n <= 14; n++)
 {
-c = j;
-if (j > 9)
+c = n;
+if (n > 9)
 {
 _putchar('1');
 c = n % 10;
@@ -20,5 +21,6 @@ c = n % 10;
 _putchar('0' + c);
 }
 _putchar('\n');
+i++;
 }
 }
