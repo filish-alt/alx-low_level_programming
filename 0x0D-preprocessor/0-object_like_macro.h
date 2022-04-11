da8d8d0a6e491ed0bd57358604ef8f12e96d6dc8
filagot <filagotshewafera@gmail.com>
2022-04-12 00:00:07 +0300

    define

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,2 +1,10 @@
-#include "main.h"
+#ifdef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
 #define SIZE 1024
+
+#endif
