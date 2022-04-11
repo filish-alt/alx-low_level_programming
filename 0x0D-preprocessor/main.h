8cb6c03b76065dc6d2c8a4101eb34643c71b9933
filagot <filagotshewafera@gmail.com>
2022-04-11 16:20:16 +0300

    define

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmy.a
+ * Auth: Efa
+ */
+#endif
